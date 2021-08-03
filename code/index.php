4cf4259ef950a4312ce73b264605ec11177e9393
shukun.wang <1419541517@qq.com>
2021-08-03 14:42:13 +0800

    init

---
+++ code/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$dsn = 'mysql:host=db;port=3306'; // host=db,这里的db其实是mysql的服务名
+$user = 'root';
+$password = '123456';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    var_dump($dbh);
+} catch (PDOException $e) {
+    echo 'Connection failed: '.$e->getMessage();
+}
+
+$redis = new Redis();
+$redis->pconnect('redis', 6379);
+
+$key = '1';
+$redis->incr($key);
+
+echo '页面浏览了：'.$redis->get($key); die;
